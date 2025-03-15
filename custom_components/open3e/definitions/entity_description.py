9ac2e6e695f9deb678be26beef3ce82f20a0d11a
Oliver <x_oliver_x@hotmail.de>
2025-03-15 22:57:48 +0000

    renamed to system info

--- custom_components/open3e/definitions/entity_description.py
+++ custom_components/open3e/definitions/entity_description.py
@@ -13,14 +13,14 @@ class Open3eEntityDescription(EntityDescription):
     poll_data_features: list[Feature] | None = None
     """Defines which features should be polled on a regular basis."""
 
-    def has_features(self, config: Open3eDataSystemInformation):
+    def has_features(self, system_information: Open3eDataSystemInformation):
         has_features = True
 
         if self.poll_data_features:
             for feature_id in map(lambda feature: feature.id, self.poll_data_features):
                 has_id_feature = False
 
-                for device in config.devices:
+                for device in system_information.devices:
                     if feature_id in map(lambda entity: entity.id, device.features):
                         has_id_feature = True
                         break
