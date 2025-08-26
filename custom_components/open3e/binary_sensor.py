e0ea393459c2fd89bb01f14513d04ce1008715cf
Oliver <x_oliver_x@hotmail.de>
2025-08-26 23:10:49 +0000

    added new sensors, binary sensors, etc.

---
+++ custom_components/open3e/binary_sensor.py
@@ -0,0 +1,53 @@
+"""Sensor platform for open3e."""
+
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant.components.binary_sensor import BinarySensorEntity
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .coordinator import Open3eDataUpdateCoordinator
+from .definitions.binary_sensors import BINARY_SENSORS, Open3eBinarySensorEntityDescription
+from .entity import Open3eEntity
+from .ha_data import Open3eDataConfigEntry
+
+
+async def async_setup_entry(
+        hass: HomeAssistant,
+        entry: Open3eDataConfigEntry,
+        async_add_entities: AddEntitiesCallback,
+) -> None:
+    async_add_entities(
+        Open3eBinarySensor(
+            coordinator=entry.runtime_data.coordinator,
+            description=description
+        )
+        for description in BINARY_SENSORS
+        if description.has_features(entry.runtime_data.coordinator.system_information)
+    )
+
+
+class Open3eBinarySensor(Open3eEntity, BinarySensorEntity):
+    entity_description: Open3eBinarySensorEntityDescription
+
+    def __init__(
+            self,
+            coordinator: Open3eDataUpdateCoordinator,
+            description: Open3eBinarySensorEntityDescription
+    ):
+        super().__init__(coordinator, description)
+
+    @property
+    def available(self):
+        """Return True if entity is available."""
+        return self._attr_is_on is not None
+
+    async def async_on_data(self, feature_id: int) -> None:
+        """Handle updated data from MQTT."""
+        self._attr_is_on = self.__transform_data(self.data[feature_id])
+        self.async_write_ha_state()
+
+    def __transform_data(self, data: Any):
+        return self.entity_description.data_transform(data)
--- custom_components/open3e/definitions/features.py
+++ custom_components/open3e/definitions/features.py
@@ -25,6 +25,8 @@ class Features:
         FlowCircuit3 = Feature(id=288, refresh_interval=5)
         FlowCircuit4 = Feature(id=290, refresh_interval=5)
         PrimaryHeatExchanger = Feature(id=320, refresh_interval=5)
+        CompressorInlet = Feature(id=321, refresh_interval=5)
+        CompressorOutlet = Feature(id=324, refresh_interval=5)
         Room1 = Feature(id=334, refresh_interval=5)
         Room2 = Feature(id=335, refresh_interval=5)
         Room3 = Feature(id=336, refresh_interval=5)
@@ -34,9 +36,16 @@ class Features:
         ProgramsCircuit2 = Feature(id=426, refresh_interval=30)
         ProgramsCircuit3 = Feature(id=428, refresh_interval=30)
         ProgramsCircuit4 = Feature(id=430, refresh_interval=30)
+        PrimaryInlet = Feature(id=1769, refresh_interval=5)
+        EngineRoom = Feature(id=1771, refresh_interval=30)
+        CompressorOil = Feature(id=1772, refresh_interval=30)
+        EconomizerLiquid = Feature(id=2333, refresh_interval=30)
+        EvaporationVapor = Feature(id=2334, refresh_interval=30)
 
     class Pressure:
         Water = Feature(id=318, refresh_interval=15)
+        CompressorInlet = Feature(id=322, refresh_interval=15)
+        CompressorOutlet = Feature(id=325, refresh_interval=15)
 
     class Energy:
         CentralHeating = Feature(id=548, refresh_interval=300)
@@ -46,7 +55,8 @@ class Features:
         PV = Feature(id=1802, refresh_interval=300)
 
     class Power:
-        Fan = Feature(id=1775, refresh_interval=30)
+        Fan1 = Feature(id=1775, refresh_interval=5)
+        Fan2 = Feature(id=1776, refresh_interval=5)
         ElectricalHeater = Feature(id=2487, refresh_interval=5)
         System = Feature(id=2488, refresh_interval=5)
         Battery = Feature(id=1836, refresh_interval=5)
@@ -58,6 +68,13 @@ class Features:
         DomesticHotWater = Feature(id=531, refresh_interval=30)
         DomesticHotWaterEfficiency = Feature(id=3029, refresh_interval=30)
         Battery = Feature(id=1664, refresh_interval=30)
+        Allengra = Feature(id=1043, refresh_interval=30)
+        HeatPumpCompressor = Feature(id=2351, refresh_interval=30)
 
     class Speed:
         CentralHeatingPump = Feature(id=381, refresh_interval=10)
+        Compressor = Feature(id=2346, refresh_interval=10)
+
+    class Position:
+        ExpansionValve1 = Feature(id=389, refresh_interval=30)
+        ExpansionValve2 = Feature(id=391, refresh_interval=30)
