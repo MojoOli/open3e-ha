2d189a344399e4510566e87ff6be6566430d6b50
Oliver <x_oliver_x@hotmail.de>
2026-04-14 19:32:54 +0000

    fixed JSON translation mappings for mixer circuits and adjusted formatting in binary_sensors configuration

--- custom_components/open3e/definitions/binary_sensors.py
+++ custom_components/open3e/definitions/binary_sensors.py
@@ -262,7 +262,7 @@ BINARY_SENSORS: tuple[Open3eBinarySensorEntityDescription, ...] = (
     ##################
 
     Open3eBinarySensorEntityDescription(
-        # device_class=None,
+        #device_class=None,
         poll_data_features=[Features.State.BackUpBox],
         key="backup_box_installed",
         translation_key="backup_box_installed",
